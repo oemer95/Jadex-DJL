f384d8a91518334860de319a3e27368be3638151
Ömer Er <38584540+oemer95@users.noreply.github.com>
2021-11-12 09:19:00 +0100

    Create env.py

---
+++ sim/env.py
@@ -0,0 +1,17 @@
+import os, sys, random, time
+import logging
+sys.path.append("../")
+
+
+from util import *
+
+logger = logging.getlogger(__name__)
+logger.setlevel(logging.DEBUG)
+logger_ch = logging.StreamHandler()
+logger_ch.setLevel(logging.DEBUG)
+logger_ch.setFormatter(logging.Formatter('%(asctime)s[%(levelname)s][%(lineno)s:%(funcName)s]||%(message)s', datefmt= '%Y-%m-%d %H:%M:%S'))
+logger.addHandler(logger_ch)
+RANDOM_SEED = 0
+
+
+
